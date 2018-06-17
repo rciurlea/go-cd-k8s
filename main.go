5cbb62f33f4bea578a7d36050dd1bf17cbe8dcd0
Radu Ciurlea <raduciurlea@gmail.com>
2018-06-17 08:34:05 +0300

    refactored main

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello!",
-		})
-	})
+	r.GET("/", index)
 	r.Run(":8080")
+}
+
+func index(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "salut!",
+	})
 }
